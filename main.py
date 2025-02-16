d2b265e17bc9647371e60a62becfeedc7017aafc
Biswajit-Rakshit <biswajitrakshit2003@gmail.com>
2025-02-17 00:42:47 +0530

    Basic Frontend Design and Implementing Form

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask.views import MethodView
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+class HomePage(MethodView):
+
+    def get(self):
+        return render_template('index.html')
+
+class BillFormPage(MethodView):
+
+    def get(self):
+        bill_form = BillForm()
+        return render_template('bill_form_page.html',bill_form=bill_form)
+
+class ResultsPage(MethodView):
+
+    def post(self):
+        bill_form = BillForm(request.form)
+        amount = bill_form.amount.data
+        return amount
+
+class BillForm(Form):
+    amount = StringField('Bill Amount: ')
+    period = StringField('Bill Period: ')
+
+    name1 = StringField('Name: ')
+    days_in_house1 = StringField('Days In The House: ')
+
+    name2 = StringField('Name: ')
+    days_in_house2 = StringField('Days In The House: ')
+
+    button = SubmitField('Calculate')
+
+app.add_url_rule('/',view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill',view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/results',view_func=ResultsPage.as_view('results_page'))
+
+app.run(debug=True)
